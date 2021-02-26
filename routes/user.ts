b1fc50c0becc73327f6c4019ef9da6f47be88efa
loveCoding01 <ishan.karmakar24@gmail.com>
2021-02-25 19:08:45 -0600

    Update

--- routes/user.ts
+++ routes/user.ts
@@ -1,5 +1,9 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router: Router = Router();
+
+router.get('/signup', async (req: Request, res: Response): Promise<void> => {
+    //
+});
 
 export default router;
--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ db;
 const app: express.Application = express();
 const PORT: number | string = process.env.PORT || 3000;
 
-app.use(router)
+app.use(express.json());
+app.use('/user', router)
 
 app.listen(PORT, (): void => console.log('Listening on port', PORT));
