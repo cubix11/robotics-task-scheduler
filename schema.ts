df03bf3f3275284e86041aee521142d02a412910
loveCoding01 <ishan.karmakar24@gmail.com>
2021-02-27 07:57:36 -0600

    test

--- schema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-const schema: Joi.Schema = Joi.object().keys({
-    username: Joi.string().trim().regex(/(^[a-zA-Z0-9_]*$)/).min(2).max(30).required(),
-    password: Joi.string().trim().regex(/(^[a-zA-Z0-9!@#$%^&*()_+]*$)/).min(8).required()
-});
-
-export default schema;
